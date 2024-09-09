9e53687c518dd43d46b60169384513f63fad5bf5
helldiving <142443321+helldiving@users.noreply.github.com>
2024-09-09 13:26:20 -0400

    Trivia changes
    Some image memes that a few friends suggested I'm not sure others would care for.

--- client/src/data/triviaData.js
+++ client/src/data/triviaData.js
@@ -1,79 +1,13 @@
 export const triviaData = [
   {
-    question: "Whose bedsheets are The Wheel of Time themed?",
-    options: ["Beta Eclipse", "Decimition", "UnboltingEnd", "HaveAKnivesDay"],
-    correctAnswer: "Beta Eclipse",
-  },
-  {
-    question:
-      "Who refused to do the fantasy football punishment two years in a row?",
-    options: ["Dectrix", "Baby Jesus ", "Decimition", "Beta Eclipse"],
-    correctAnswer: "Baby Jesus",
-  },
-  {
-    question: "Which two blokes yap about the Stormlight Archive?",
+    question: "Example question: who is your favorite co-worker?",
     options: [
-      "HaveAKnivesDay and Jameson",
-      "Beta Eclipse and Dectrix ",
-      "Baby Jesus and Millie",
-      "UnboltingEnd and Sean",
+      "The CEO's son-in-law who insists on full suits for casual fridays",
+      "Margaret from accounting",
+      "The one guy on my team who takes a week to get back to me and then spams me with pings",
+      "The office coffee machine is what fuels this fire",
     ],
-    correctAnswer: "Beta Eclipse and Dectrix",
-  },
-  {
-    question: "Who uses their freewill to NOT play pickleball with the boys?",
-    options: ["Jameson", "UnboltingEnd", "Beta Eclipse", "HaveAKnivesDay"],
-    correctAnswer: "Beta Eclipse",
-  },
-  {
-    question: "Which amigo drives with no shoes (whack)? ",
-    options: ["Jameson", "Decimition", "Dectrix", "HaveAKnivesDay"],
-    correctAnswer: "Jameson",
-  },
-  {
-    question: "Who could not ride a Dune Sandworm? ",
-    options: ["UnboltingEnd", "HaveAKnivesDay", "Jameson", "Dectrix"],
-    correctAnswer: "UnboltingEnd",
-  },
-  {
-    question: "How did The Rat House get its name?",
-    options: [
-      "One of the amigos living there was a rat",
-      "Underground rat fighting ring",
-      "Blokes love cheese",
-      "One of the 10 trials was defeating a family of rats",
-    ],
-    correctAnswer: "One of the 10 trials was defeating a family of rats",
-  },
-  {
-    question: "Which shaved little yard gnome coined the term lil amigo?",
-    options: ["HaveAKnivesDay", "Decimition", "Beta Eclipse", "Jameson"],
-    correctAnswer: "Decimition",
-  },
-  {
-    question: "Who is Thomas S. aka JusticeAlwaysPrevails?",
-    options: [
-      "Lord of the Rings bloke",
-      "13th Disciple",
-      "UnboltingEnd's rareform",
-      "The light that exposed the world",
-    ],
-    correctAnswer: "The light that exposed the world",
-  },
-  {
-    question: "Most memorable home of the blokes?",
-    options: ["The Rat House", "Egg Townhouse", "Nole Quarters"],
-    correctAnswer: "Nole Quarters",
-  },
-  {
-    question: "Game of Thrones biggest fan?",
-    options: [
-      "HaveAKnivesDay",
-      "HaveAKnivesDay",
-      "HaveAKnivesDay",
-      "HaveAKnivesDay",
-    ],
-    correctAnswer: "HaveAKnivesDay",
+    correctAnswer: "The office coffee machine is what fuels this fire",
   },
 
   // Add more trivia questions and answers here
